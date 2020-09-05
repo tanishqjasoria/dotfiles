528c45259bdfba6f10c334302412d24a6ee4589d
Tanishq Jasoria <jasoriatanishq@gmail.com>
2020-09-05 08:18:26 +0530

    Add dotfiles setup script

--- setup.sh
+++ setup.sh
@@ -1,36 +1,31 @@
-sudo apt update
-sudo apt -y upgrade
-sudo apt install -y vim
+#!/bin/bash
+dotfilesDir=$(pwd)
 
-sudo apt install -y resolvconf
-sudo systemctl start resolvconf.service
-sudo systemctl enable resolvconf.service
+function linkDotfile {
+  dest="${HOME}/${1}"
+  dateStr=$(date +%Y-%m-%d-%H%M)
 
-echo "nameserver 1.1.1.1" | sudo tee /etc/resolvconf/resolv.conf.d/head
-echo "nameserver 8.8.8.8" | sudo tee /etc/resolvconf/resolv.conf.d/head
-echo "nameserver 8.8.4.4" | sudo tee /etc/resolvconf/resolv.conf.d/head
-echo "nameserver 1.0.0.1" | sudo tee /etc/resolvconf/resolv.conf.d/head
+  if [ -h ~/${1} ]; then
+    # Existing symlink
+    echo "Removing existing symlink: ${dest}"
+    rm ${dest}
 
-sudo systemctl restart resolvconf
+  elif [ -f "${dest}" ]; then
+    # Existing file
+    echo "Backing up existing file: ${dest}"
+    mv ${dest}{,.${dateStr}}
 
-echo "[Installing]: Brave Browser"
-sudo apt install -y apt-transport-https curl
+  elif [ -d "${dest}" ]; then
+    # Existing dir
+    echo "Backing up existing dir: ${dest}"
+    mv ${dest}{,.${dateStr}}
+  fi
 
-curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
+  echo "Creating new symlink: ${dest}"
+  ln -s ${dotfilesDir}/${1} ${dest}
+}
 
-echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-
-sudo apt update
-
-sudo apt install -y brave-browser
-
-echo "[]Installing]: Rust"
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-
-sudo snap refresh
-sudo snap install slack
-sudo snap install pycharm-professional
-sudo snap install intellij-idea-ultimate
-sudo snap install clion
-sudo snap install webstorm
-
+linkDotfile .vim
+linkDotfile .vimrc
+linkDotfile .bashrc
+linkDotfile .gitconfig
