ea08a6ac49ea4205fe22e54380a4f38db624a39b
Tanishq Jasoria <jasoriatanishq@gmail.com>
2020-11-18 19:25:53 +0530

    ADD docker and anaconda

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1,6 +1,6 @@
 sudo apt update
 sudo apt -y upgrade
-sudo apt install -y vim
+sudo apt install -y vim git gcc code nvim 
 
 sudo apt install -y resolvconf
 sudo systemctl start resolvconf.service
@@ -27,7 +27,30 @@ sudo apt install -y brave-browser
 echo "[]Installing]: Rust"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
+sudo apt install -y node
+
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo usermod -ag docker eurus
+
+sudo apt-get install libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6
+wget https://repo.anaconda.com/archive/Anaconda3-2020.07-Linux-x86_64.sh
+./Anaconda3-2020.07-Linux-x86_64.sh
+
 sudo snap refresh
+sudo snap install spotify
 sudo snap install slack --classic
 sudo snap install pycharm-professional --classic
 sudo snap install intellij-idea-ultimate --classic
